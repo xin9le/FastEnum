1646a6dd51783b639fd55a6b7a161c85876d4469
Takaaki Suzuki <xin9le@live.jp>
2019-09-04 02:50:51 +0900

    Add sbyte enum test

---
+++ tests/FastEnum.Tests/Enums.cs
@@ -0,0 +1,65 @@
+﻿namespace FastEnum.Tests
+{
+    public enum SByteEnum : sbyte
+    {
+        MinValue = sbyte.MinValue,
+        Zero = 0,
+        MaxValue = sbyte.MaxValue,
+    }
+
+
+    public enum ByteEnum : byte
+    {
+        MinValue = byte.MinValue,
+        Zero = 0,
+        MaxValue = byte.MaxValue,
+    }
+
+
+    public enum Int16Enum : short
+    {
+        MinValue = short.MinValue,
+        Zero = 0,
+        MaxValue = short.MaxValue,
+    }
+
+
+    public enum UInt16Enum : ushort
+    {
+        MinValue = ushort.MinValue,
+        Zero = 0,
+        MaxValue = ushort.MaxValue,
+    }
+
+
+    public enum Int32Enum : int
+    {
+        MinValue = int.MinValue,
+        Zero = 0,
+        MaxValue = int.MaxValue,
+    }
+
+
+    public enum UInt32Enum : uint
+    {
+        MinValue = uint.MinValue,
+        Zero = 0,
+        MaxValue = uint.MaxValue,
+    }
+
+
+    public enum Int64Enum : long
+    {
+        MinValue = long.MinValue,
+        Zero = 0,
+        MaxValue = long.MaxValue,
+    }
+
+
+    public enum UInt64Enum : ulong
+    {
+        MinValue = ulong.MinValue,
+        Zero = 0,
+        MaxValue = ulong.MaxValue,
+    }
+}
---
+++ tests/FastEnum.Tests/SByteTest.cs
@@ -0,0 +1,76 @@
+﻿using FluentAssertions;
+using Xunit;
+
+
+
+namespace FastEnum.Tests
+{
+    public class SByteTest
+    {
+        [Fact]
+        public void Type()
+        {
+            var t = FastEnum<SByteEnum>.Type;
+            t.Should().Be<SByteEnum>();
+        }
+
+
+        [Fact]
+        public void UnderlyingType()
+        {
+            var t = FastEnum<SByteEnum>.UnderlyingType;
+            t.Should().Be<sbyte>();
+        }
+
+
+        [Fact]
+        public void Values()
+        {
+            var expect = new[]
+            {
+                sbyte.MinValue,
+                default,
+                sbyte.MaxValue,
+            };
+            var actual = FastEnum<SByteEnum>.Values;
+            actual.Should().BeEquivalentTo(expect);
+        }
+
+
+        [Fact]
+        public void Names()
+        {
+            var expect = new[]
+            {
+                nameof(SByteEnum.MinValue),
+                nameof(SByteEnum.Zero),
+                nameof(SByteEnum.MaxValue),
+            };
+            var actual = FastEnum<SByteEnum>.Names;
+            actual.Should().BeEquivalentTo(expect);
+        }
+
+
+        [Fact]
+        public void Members()
+        {
+            var expect = new[]
+            {
+                new Member<SByteEnum>(SByteEnum.Zero),
+                new Member<SByteEnum>(SByteEnum.MaxValue),
+                new Member<SByteEnum>(SByteEnum.MinValue),
+            };
+            var actual = FastEnum<SByteEnum>.Members;
+
+            actual.Length.Should().Be(expect.Length);
+            for (var i = 0; i < expect.Length; i++)
+            {
+                var a = actual[i];
+                var e = expect[i];
+                a.Value.Should().Be(e.Value);
+                a.Name.Should().Be(e.Name);
+                a.FieldInfo.Should().Be(e.FieldInfo);
+            }
+        }
+    }
+}

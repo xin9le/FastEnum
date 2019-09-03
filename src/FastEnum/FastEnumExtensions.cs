8430ee057c89cf0a77477e4f50406837b10b3912
Takaaki Suzuki <xin9le@live.jp>
2019-09-03 23:53:26 +0900

    Change to type code switching

--- src/FastEnum/FastEnumExtensions.cs
+++ src/FastEnum/FastEnumExtensions.cs
@@ -43,56 +43,67 @@ namespace FastEnum
         public static bool Has<T>(this T value, T flags)
             where T : Enum
         {
-            var ut = FastEnum<T>.UnderlyingType;
-            if (ut == typeof(byte))
+            switch (value.GetTypeCode())
             {
-                var v = Unsafe.As<T, byte>(ref value);
-                var f = Unsafe.As<T, byte>(ref flags);
-                return (v & f) == f;
+                case TypeCode.Byte:
+                    {
+                        var v = Unsafe.As<T, byte>(ref value);
+                        var f = Unsafe.As<T, byte>(ref flags);
+                        return (v & f) == f;
+                    }
+
+                case TypeCode.SByte:
+                    {
+                        var v = Unsafe.As<T, sbyte>(ref value);
+                        var f = Unsafe.As<T, sbyte>(ref flags);
+                        return (v & f) == f;
+                    }
+
+                case TypeCode.Int16:
+                    {
+                        var v = Unsafe.As<T, short>(ref value);
+                        var f = Unsafe.As<T, short>(ref flags);
+                        return (v & f) == f;
+                    }
+
+                case TypeCode.UInt16:
+                    {
+                        var v = Unsafe.As<T, ushort>(ref value);
+                        var f = Unsafe.As<T, ushort>(ref flags);
+                        return (v & f) == f;
+                    }
+
+                case TypeCode.Int32:
+                    {
+                        var v = Unsafe.As<T, int>(ref value);
+                        var f = Unsafe.As<T, int>(ref flags);
+                        return (v & f) == f;
+                    }
+
+                case TypeCode.UInt32:
+                    {
+                        var v = Unsafe.As<T, uint>(ref value);
+                        var f = Unsafe.As<T, uint>(ref flags);
+                        return (v & f) == f;
+                    }
+
+                case TypeCode.Int64:
+                    {
+                        var v = Unsafe.As<T, long>(ref value);
+                        var f = Unsafe.As<T, long>(ref flags);
+                        return (v & f) == f;
+                    }
+
+                case TypeCode.UInt64:
+                    {
+                        var v = Unsafe.As<T, ulong>(ref value);
+                        var f = Unsafe.As<T, ulong>(ref flags);
+                        return (v & f) == f;
+                    }
+
+                default:
+                    throw new InvalidOperationException();
             }
-            else if (ut == typeof(sbyte))
-            {
-                var v = Unsafe.As<T, sbyte>(ref value);
-                var f = Unsafe.As<T, sbyte>(ref flags);
-                return (v & f) == f;
-            }
-            else if (ut == typeof(short))
-            {
-                var v = Unsafe.As<T, short>(ref value);
-                var f = Unsafe.As<T, short>(ref flags);
-                return (v & f) == f;
-            }
-            else if (ut == typeof(ushort))
-            {
-                var v = Unsafe.As<T, ushort>(ref value);
-                var f = Unsafe.As<T, ushort>(ref flags);
-                return (v & f) == f;
-            }
-            else if (ut == typeof(int))
-            {
-                var v = Unsafe.As<T, int>(ref value);
-                var f = Unsafe.As<T, int>(ref flags);
-                return (v & f) == f;
-            }
-            else if (ut == typeof(uint))
-            {
-                var v = Unsafe.As<T, uint>(ref value);
-                var f = Unsafe.As<T, uint>(ref flags);
-                return (v & f) == f;
-            }
-            else if (ut == typeof(long))
-            {
-                var v = Unsafe.As<T, long>(ref value);
-                var f = Unsafe.As<T, long>(ref flags);
-                return (v & f) == f;
-            }
-            else if (ut == typeof(ulong))
-            {
-                var v = Unsafe.As<T, ulong>(ref value);
-                var f = Unsafe.As<T, ulong>(ref flags);
-                return (v & f) == f;
-            }
-            throw new InvalidOperationException();
         }
     }
 }

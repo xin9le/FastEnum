ee5ed048897e9a78d5350e0cdd0e5f0198c4c494
Takaaki Suzuki <xin9le@live.jp>
2019-09-03 17:51:45 +0900

    Add concept code

---
+++ src/FastEnum/FastEnum.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+
+
+namespace FastEnum
+{
+    public static class FastEnum<T>
+        where T : Enum
+    {
+        public sealed class Member
+        {
+            public T Value { get; }
+            public string Name { get; }
+            public FieldInfo FieldInfo { get; }
+            internal Member(T value)
+            {
+                var type = typeof(T);
+                this.Value = value;
+                this.Name = Enum.GetName(type, value);
+                this.FieldInfo = type.GetField(this.Name);
+            }
+        }
+
+
+        public static Type Type { get; }
+        public static Type UnderlyingType { get; }
+        public static T[] Values { get; }
+        public static string[] Names { get; }
+        public static Member[] Members { get; }
+        internal static Dictionary<T, Member> MemberByValue { get; }
+        internal static Dictionary<string, Member> MemberByName { get; }
+
+
+        static FastEnum()
+        {
+            Type = typeof(T);
+            UnderlyingType = Enum.GetUnderlyingType(Type);
+            Values = Enum.GetValues(Type) as T[];
+            Names = Enum.GetNames(Type);
+            Members = Values.Select(x => new Member(x)).ToArray();
+            MemberByValue = Members.ToDictionary(x => x.Value);
+            MemberByName = Members.ToDictionary(x => x.Name);
+        }
+
+
+        public static bool IsDefined(T value)
+            => MemberByValue.ContainsKey(value);
+
+
+        public static T Parse(string name)
+            => MemberByName.TryGetValue(name, out var member)
+            ? member.Value
+            : throw new ArgumentException(nameof(name));
+
+
+        public static bool TryParse(string name, out T value)
+        {
+            if (MemberByName.TryGetValue(name, out var member))
+            {
+                value = member.Value;
+                return true;
+            }
+            value = default;
+            return false;
+        }
+    }
+}
---
+++ src/FastEnum/FastEnumExtensions.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+
+
+namespace FastEnum
+{
+    public static class FastEnumExtensions
+    {
+        public static FastEnum<T>.Member ToMember<T>(this T value)
+            where T : Enum
+            => FastEnum<T>.MemberByValue[value];
+
+
+        public static string ToName<T>(this T value)
+            where T : Enum
+            => value.ToMember().Name;
+
+
+        public static bool Has<T>(this T value, T flags)
+            where T : Enum
+        {
+            var ut = FastEnum<T>.UnderlyingType;
+            if (ut == typeof(byte))
+            {
+                var v = Unsafe.As<T, byte>(ref value);
+                var f = Unsafe.As<T, byte>(ref flags);
+                return (v & f) == f;
+            }
+            else if (ut == typeof(sbyte))
+            {
+                var v = Unsafe.As<T, sbyte>(ref value);
+                var f = Unsafe.As<T, sbyte>(ref flags);
+                return (v & f) == f;
+            }
+            else if (ut == typeof(short))
+            {
+                var v = Unsafe.As<T, short>(ref value);
+                var f = Unsafe.As<T, short>(ref flags);
+                return (v & f) == f;
+            }
+            else if (ut == typeof(ushort))
+            {
+                var v = Unsafe.As<T, ushort>(ref value);
+                var f = Unsafe.As<T, ushort>(ref flags);
+                return (v & f) == f;
+            }
+            else if (ut == typeof(int))
+            {
+                var v = Unsafe.As<T, int>(ref value);
+                var f = Unsafe.As<T, int>(ref flags);
+                return (v & f) == f;
+            }
+            else if (ut == typeof(uint))
+            {
+                var v = Unsafe.As<T, uint>(ref value);
+                var f = Unsafe.As<T, uint>(ref flags);
+                return (v & f) == f;
+            }
+            else if (ut == typeof(long))
+            {
+                var v = Unsafe.As<T, long>(ref value);
+                var f = Unsafe.As<T, long>(ref flags);
+                return (v & f) == f;
+            }
+            else if (ut == typeof(ulong))
+            {
+                var v = Unsafe.As<T, ulong>(ref value);
+                var f = Unsafe.As<T, ulong>(ref flags);
+                return (v & f) == f;
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
+        }
+    }
+}

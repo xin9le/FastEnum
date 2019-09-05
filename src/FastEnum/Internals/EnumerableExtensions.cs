fee68415639a224be5cf6a29bca82e8ea1065629
Takaaki Suzuki <xin9le@live.jp>
2019-09-05 18:04:05 +0900

    Add FrozenDictionary

---
+++ src/FastEnum/Internals/EnumerableExtensions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+
+namespace FastEnum.Internals
+{
+    internal static class EnumerableExtensions
+    {
+        /// <summary>
+        /// Converts to <see cref="FrozenDictionary{TKey, TValue}"/>.
+        /// </summary>
+        /// <typeparam name="TKey"></typeparam>
+        /// <typeparam name="TValue"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="keySelector"></param>
+        /// <returns></returns>
+        public static FrozenDictionary<TKey, TValue> ToFrozenDictionary<TKey, TValue>(this IEnumerable<TValue> source, Func<TValue, TKey> keySelector)
+            => FrozenDictionary<TKey, TValue>.Create(source, keySelector);
+
+
+        /// <summary>
+        /// Converts to <see cref="FrozenDictionary{TKey, TValue}"/>.
+        /// </summary>
+        /// <typeparam name="TKey"></typeparam>
+        /// <typeparam name="TValue"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="keySelector"></param>
+        /// <param name="valueSelector"></param>
+        /// <returns></returns>
+        public static FrozenDictionary<TKey, TValue> ToFrozenDictionary<TSource, TKey, TValue>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector, Func<TSource, TValue> valueSelector)
+            => FrozenDictionary<TKey, TValue>.Create(source, keySelector, valueSelector);
+
+
+        /// <summary>
+        /// Gets collection count if <see cref="source"/> is materialized, otherwise null.
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static int? CountIfMaterialized<T>(this IEnumerable<T> source)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+
+            if (source == Enumerable.Empty<T>()) return 0;
+            if (source == Array.Empty<T>()) return 0;
+            if (source is ICollection<T> a) return a.Count;
+            if (source is IReadOnlyCollection<T> b) return b.Count;
+
+            return null;
+        }
+    }
+}

51a10b710aaf549a6784631fcdd2d142956e0ad9
Takaaki Suzuki <xin9le@live.jp>
2019-09-06 03:01:04 +0900

    Add FrozenDictionary test.

---
+++ src/FastEnum.Tests/Cases/FrozenDictionaryTest.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+using FastEnum.Internals;
+using FluentAssertions;
+using Xunit;
+
+
+
+namespace FastEnum.Tests.Cases
+{
+    public class FrozenDictionaryTest
+    {
+        private const int Count = 100;
+
+
+        [Fact]
+        public void GenericsKey()
+        {
+            var values = Enumerable.Range(0, Count);
+            var dic = values.ToFrozenDictionary(x => x);
+
+            dic.Count.Should().Be(Count);
+            dic.TryGetValue(-1, out _).Should().BeFalse();
+            foreach (var x in values)
+            {
+                dic.TryGetValue(x, out var result).Should().BeTrue();
+                result.Should().Be(x);
+            }
+        }
+
+
+        [Fact]
+        public void IntKey()
+        {
+            var values = Enumerable.Range(0, Count);
+            var dic = values.ToIntKeyFrozenDictionary(x => x);
+
+            dic.Count.Should().Be(Count);
+            dic.TryGetValue(-1, out _).Should().BeFalse();
+            foreach (var x in values)
+            {
+                dic.TryGetValue(x, out var result).Should().BeTrue();
+                result.Should().Be(x);
+            }
+        }
+
+
+        [Fact]
+        public void StringKey()
+        {
+            var values
+                = Enumerable.Range(0, Count)
+                .Select(_ => Guid.NewGuid().ToString())
+                .ToArray();
+            var dic = values.ToStringKeyFrozenDictionary(x => x);
+
+            dic.Count.Should().Be(Count);
+            dic.TryGetValue(string.Empty, out _).Should().BeFalse();
+            foreach (var x in values)
+            {
+                dic.TryGetValue(x, out var result).Should().BeTrue();
+                result.Should().Be(x);
+            }
+        }
+    }
+}

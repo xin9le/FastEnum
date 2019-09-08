0428eae36d7eecbc180ad79d1d4798149a24cac6
Takaaki Suzuki <xin9le@live.jp>
2019-09-09 03:42:22 +0900

    Add materialized extension method

--- src/FastEnum/Internals/EnumerableExtensions.cs
+++ src/FastEnum/Internals/EnumerableExtensions.cs
@@ -91,6 +91,7 @@ namespace FastEnum.Internals
         /// <summary>
         /// Gets collection count if <see cref="source"/> is materialized, otherwise null.
         /// </summary>
+        /// <typeparam name="T"></typeparam>
         /// <param name="source"></param>
         /// <returns></returns>
         public static int? CountIfMaterialized<T>(this IEnumerable<T> source)
@@ -104,6 +105,26 @@ namespace FastEnum.Internals
             if (source is IReadOnlyCollection<T> b) return b.Count;
 
             return null;
+        }
+
+
+        /// <summary>
+        /// Gets collection if <see cref="source"/> is materialized, otherwise ToArray();ed collection.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="nullToEmpty"></param>
+        public static IEnumerable<T> Materialize<T>(this IEnumerable<T> source, bool nullToEmpty = true)
+        {
+            if (source == null)
+            {
+                if (nullToEmpty)
+                    return Enumerable.Empty<T>();
+                throw new ArgumentNullException(nameof(source));
+            }
+            if (source is ICollection<T>) return source;
+            if (source is IReadOnlyCollection<T>) return source;
+            return source.ToArray();
         }
     }
 }

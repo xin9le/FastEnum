c1454da06843a54b203ab2fba1a18c5b0b11fed8
Takaaki Suzuki <xin9le@live.jp>
2019-10-20 20:43:05 +0900

    Fix benchmark logic

--- src/FastEnum.Benchmark/Scenarios/StringEqualsBenchmark.cs
+++ src/FastEnum.Benchmark/Scenarios/StringEqualsBenchmark.cs
@@ -7,22 +7,22 @@ namespace FastEnumUtility.Benchmark.Scenarios
 {
     public class StringEqualsBenchmark
     {
-        private const string Lower = "abcdefghijklmnopqrstuvwxyz";
-        private const string Upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        private string Lower { get; set; }
+        private string Upper { get; set; }
 
 
         [GlobalSetup]
         public void Setup()
-        {}
+        {
+            var text = Guid.NewGuid().ToString();
+            Lower = text.ToLower();
+            Upper = text.ToUpper();
+        }
 
 
         [Benchmark(Baseline = true)]
         public bool String_OrdinalIgnoreCase()
-        {
-            var lower = Lower.AsSpan();
-            var upper = Upper.AsSpan();
-            return Lower.Equals(Upper, StringComparison.OrdinalIgnoreCase);
-        }
+            => Lower.Equals(Upper, StringComparison.OrdinalIgnoreCase);
 
 
         [Benchmark]
@@ -36,11 +36,7 @@ namespace FastEnumUtility.Benchmark.Scenarios
 
         [Benchmark]
         public bool String_InvariantCultureIgnoreCase()
-        {
-            var lower = Lower.AsSpan();
-            var upper = Upper.AsSpan();
-            return Lower.Equals(Upper, StringComparison.InvariantCultureIgnoreCase);
-        }
+            => Lower.Equals(Upper, StringComparison.InvariantCultureIgnoreCase);
 
 
         [Benchmark]

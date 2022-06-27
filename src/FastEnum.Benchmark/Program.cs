b4129d935bf1ae55d432118906a6d255456923b8
Takaaki Suzuki <xin9le@live.jp>
2022-06-27 14:35:43 +0900

    Apply top level statement

--- src/FastEnum.Benchmark/Program.cs
+++ src/FastEnum.Benchmark/Program.cs
@@ -1,33 +1,26 @@
 ﻿using BenchmarkDotNet.Running;
+using FastEnumUtility.Benchmark;
 using FastEnumUtility.Benchmark.Scenarios;
 
-namespace FastEnumUtility.Benchmark;
 
 
-
-class Program
+var switcher = new BenchmarkSwitcher(new[]
 {
-    static void Main(string[] args)
-    {
-        var switcher = new BenchmarkSwitcher(new[]
-        {
-            typeof(GetValuesBenchmark),
-            typeof(GetNamesBenchmark),
-            typeof(GetNameBenchmark),
-            typeof(TryParseBenchmark),
-            typeof(TryParseIgnoreCaseBenchmark),
-            typeof(IsDefinedByteBenchmark),
-            typeof(IsDefinedEnumBenchmark),
-            typeof(IsDefinedStringBenchmark),
-            typeof(ToStringBenchmark),
-            typeof(DictionaryEnumKeyBenchmark),
-            typeof(DictionaryInt32KeyBenchmark),
-            typeof(DictionaryStringKeyBenchmark),
-            typeof(EnumMemberAttributeBenchmark),
-            typeof(ForEachBenchmark),
-            typeof(StringEqualsBenchmark),
-            typeof(InitializeBenchmark),
-        });
-        switcher.Run(args, new BenchmarkConfig());
-    }
-}
+    typeof(GetValuesBenchmark),
+    typeof(GetNamesBenchmark),
+    typeof(GetNameBenchmark),
+    typeof(TryParseBenchmark),
+    typeof(TryParseIgnoreCaseBenchmark),
+    typeof(IsDefinedByteBenchmark),
+    typeof(IsDefinedEnumBenchmark),
+    typeof(IsDefinedStringBenchmark),
+    typeof(ToStringBenchmark),
+    typeof(DictionaryEnumKeyBenchmark),
+    typeof(DictionaryInt32KeyBenchmark),
+    typeof(DictionaryStringKeyBenchmark),
+    typeof(EnumMemberAttributeBenchmark),
+    typeof(ForEachBenchmark),
+    typeof(StringEqualsBenchmark),
+    typeof(InitializeBenchmark),
+});
+switcher.Run(args, new BenchmarkConfig());

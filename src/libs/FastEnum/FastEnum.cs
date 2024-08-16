8074fdfd15b5701ffb464928e0567c8cb7d4912c
Takaaki Suzuki <xin9le@live.jp>
2024-08-16 14:57:20 +0900

    Add FastEnum.cs

---
+++ src/libs/FastEnum/FastEnum.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FastEnumUtility;
+
+
+
+/// <summary>
+/// Provides high performance utilities for <see cref="Enum"/> type.
+/// </summary>
+public static class FastEnum
+{
+}

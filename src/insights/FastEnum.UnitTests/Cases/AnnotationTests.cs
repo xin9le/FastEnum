458e5a0a8b27ddc79cdb18ee13fc32400ac50de2
Takaaki Suzuki <xin9le@live.jp>
2024-08-19 00:16:08 +0900

    Add more test cases

---
+++ src/insights/FastEnum.UnitTests/Cases/AnnotationTests.cs
@@ -0,0 +1,87 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TEnum = FastEnumUtility.UnitTests.Models.AnnotationEnum;
+
+namespace FastEnumUtility.UnitTests.Cases;
+
+
+
+[TestClass]
+public class AnnotationTests
+{
+    [TestMethod]
+    public void IsFlags()
+        => FastEnum.IsFlags<TEnum>().Should().Be(true);
+
+
+    [TestMethod]
+    public void EnumMemberAttribute()
+    {
+        {
+            var member = TEnum.Zero.ToMember()!;
+            var attr = member.EnumMemberAttribute!;
+            attr.Should().NotBeNull();
+            attr.IsValueSetExplicitly.Should().BeTrue();
+            attr.Value.Should().Be("_zero_");
+        }
+        {
+            var member = TEnum.One.ToMember()!;
+            var attr = member.EnumMemberAttribute!;
+            attr.Should().NotBeNull();
+            attr.IsValueSetExplicitly.Should().BeFalse();
+            attr.Value.Should().BeNull();
+        }
+        {
+            var member = TEnum.Two.ToMember()!;
+            var attr = member.EnumMemberAttribute!;
+            attr.Should().BeNull();
+        }
+    }
+
+
+    [TestMethod]
+    public void GetEnumMemberValue()
+    {
+        TEnum.Zero.GetEnumMemberValue().Should().Be("_zero_");
+        TEnum.One.GetEnumMemberValue().Should().BeNull();
+        TEnum.Two.GetEnumMemberValue(throwIfNotFound: false).Should().BeNull();
+        FluentActions
+            .Invoking(static () => TEnum.Two.GetEnumMemberValue(throwIfNotFound: true))
+            .Should()
+            .Throw<NotFoundException>();
+        FluentActions
+            .Invoking(static () =>
+            {
+                const TEnum undefined = (TEnum)123;
+                undefined.GetEnumMemberValue(throwIfNotFound: true);
+            })
+            .Should()
+            .Throw<NotFoundException>();
+    }
+
+
+    [TestMethod]
+    public void GetLabel()
+    {
+        TEnum.Zero.GetLabel(0).Should().Be("ぜろ");
+        TEnum.Zero.GetLabel(1).Should().Be("零");
+
+        TEnum.One.GetLabel(0).Should().Be("いち");
+        TEnum.One.GetLabel(1).Should().Be("壱");
+
+        TEnum.Two.GetLabel(0, throwIfNotFound: false).Should().BeNull();
+        FluentActions
+            .Invoking(static () => TEnum.Two.GetLabel(0, throwIfNotFound: true))
+            .Should()
+            .Throw<NotFoundException>();
+
+        TEnum.Four.GetLabel(2).Should().BeNull();
+
+        const TEnum undefined = (TEnum)123;
+        undefined.GetLabel(0, throwIfNotFound: false).Should().BeNull();
+        FluentActions
+            .Invoking(static () => undefined.GetLabel(0, throwIfNotFound: true))
+            .Should()
+            .Throw<NotFoundException>();
+    }
+}
---
+++ src/insights/FastEnum.UnitTests/Cases/EmptyTests.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TEnum = FastEnumUtility.UnitTests.Models.EmptyEnum;
+
+namespace FastEnumUtility.UnitTests.Cases;
+
+
+
+[TestClass]
+public class EmptyTests
+{
+    [TestMethod]
+    public void GetValues()
+        => FastEnum.GetValues<TEnum>().Should().BeEmpty();
+
+
+    [TestMethod]
+    public void GetNames()
+        => FastEnum.GetNames<TEnum>().Should().BeEmpty();
+
+
+    [TestMethod]
+    public void GetName()
+        => FastEnum.GetName<TEnum>(default).Should().BeNull();
+
+
+    [TestMethod]
+    public void GetMembers()
+        => FastEnum.GetMembers<TEnum>().Should().BeEmpty();
+
+
+    [TestMethod]
+    public void GetMember()
+        => FastEnum.GetMember<TEnum>(default).Should().BeNull();
+
+
+    [TestMethod]
+    public void GetMinValue()
+        => FastEnum.GetMinValue<TEnum>().Should().BeNull();
+
+
+    [TestMethod]
+    public void GetMaxValue()
+        => FastEnum.GetMaxValue<TEnum>().Should().BeNull();
+
+
+    [TestMethod]
+    public void IsEmpty()
+        => FastEnum.IsEmpty<TEnum>().Should().BeTrue();
+
+
+    [TestMethod]
+    public void IsContinuous()
+        => FastEnum.IsContinuous<TEnum>().Should().BeFalse();
+
+
+    [TestMethod]
+    public void IsDefined()
+    {
+        FastEnum.IsDefined((TEnum)123).Should().BeFalse();
+        FastEnum.IsDefined<TEnum>("123").Should().BeFalse();
+        /*
+        FluentActions
+            .Invoking(static () => FastEnum.IsDefined<TEnum>((sbyte)123))
+            .Should()
+            .Throw<ArgumentException>();
+        */
+    }
+
+
+    [TestMethod]
+    public void Parse()
+        => FluentActions.Invoking(static () => FastEnum.Parse<TEnum>("ABCDE")).Should().Throw<ArgumentException>();
+
+
+    [TestMethod]
+    public void ParseIgnoreCase()
+        => FluentActions.Invoking(static () => FastEnum.Parse<TEnum>("ABCDE", true)).Should().Throw<ArgumentException>();
+
+
+    [TestMethod]
+    public void TryParse()
+        => FastEnum.TryParse<TEnum>("ABCDE", out var _).Should().BeFalse();
+
+
+    [TestMethod]
+    public void TryParseIgnoreCase()
+        => FastEnum.TryParse<TEnum>("ABCDE", true, out var _).Should().BeFalse();
+}

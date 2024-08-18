3a321967ec95f39efa100d482e61969d611464cb
Takaaki Suzuki <xin9le@live.jp>
2024-08-18 22:27:43 +0900

    Add NotFoundException

---
+++ src/libs/FastEnum/NotFoundException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FastEnumUtility;
+
+
+
+/// <summary>
+/// Represents an <see cref="Exception"/> that is thrown when not found something.
+/// </summary>
+public sealed class NotFoundException : Exception
+{
+    #region Constructors
+    /// <summary>
+    /// Creates instance.
+    /// </summary>
+    /// <param name="message"></param>
+    internal NotFoundException(string message)
+        : base(message)
+    { }
+    #endregion
+}

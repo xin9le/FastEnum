7ad600bc20a712c9bfedf1a380c1a161f6ef1b61
Takaaki Suzuki <xin9le@live.jp>
2024-08-19 14:56:13 +0900

    Add .ToString() benchmark for defined value

--- src/insights/FastEnum.Benchmarks/Program.cs
+++ src/insights/FastEnum.Benchmarks/Program.cs
@@ -1,10 +1,13 @@
-﻿using System;
-using BenchmarkDotNet.Running;
+﻿using BenchmarkDotNet.Running;
 using FastEnumUtility.Benchmarks.Internals;
+using FastEnumUtility.Benchmarks.Scenarios;
 
 
 
 //var config = BenchmarkConfig.ForDefault();
 var config = BenchmarkConfig.ForShortRun();
-var switcher = new BenchmarkSwitcher(Type.EmptyTypes);
+var switcher = new BenchmarkSwitcher(
+[
+    typeof(Scenario_ToString_Defined),
+]);
 switcher.Run(args, config);

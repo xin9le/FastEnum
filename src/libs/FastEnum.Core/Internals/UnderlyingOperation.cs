bbcfd720fffb4c2abdeaa36f7542b7eaec3a98f6
Takaaki Suzuki <xin9le@live.jp>
2024-08-19 22:05:22 +0900

    Remove unnecessary using alias

--- src/libs/FastEnum.Core/Internals/UnderlyingOperation.cs
+++ src/libs/FastEnum.Core/Internals/UnderlyingOperation.cs
@@ -6,7 +6,6 @@
 using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
-using System.Numerics;
 using System.Runtime.CompilerServices;
 
 namespace FastEnumUtility.Internals;

511b3150bbd04c79099fcb5f3092ee417aee94c6
Takaaki Suzuki <xin9le@live.jp>
2024-08-19 14:31:58 +0900

    Implement benchmark startup

---
+++ src/insights/FastEnum.Benchmarks/Internals/BenchmarkConfig.cs
@@ -0,0 +1,30 @@
+﻿using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Exporters;
+using BenchmarkDotNet.Jobs;
+
+namespace FastEnumUtility.Benchmarks.Internals;
+
+
+
+internal static class BenchmarkConfig
+{
+    public static IConfig ForDefault()
+    {
+        return ManualConfig.CreateEmpty()
+            .AddExporter(MarkdownExporter.GitHub)
+            .AddDiagnoser(MemoryDiagnoser.Default)
+            .AddJob(Job.Default)
+            .AddColumn(StatisticColumn.AllStatistics);
+    }
+
+
+    public static IConfig ForShortRun()
+    {
+        return ManualConfig.CreateEmpty()
+            .AddExporter(MarkdownExporter.GitHub)
+            .AddDiagnoser(MemoryDiagnoser.Default)
+            .AddJob(Job.ShortRun.WithWarmupCount(0).WithIterationCount(1));
+    }
+}
--- src/insights/FastEnum.Benchmarks/Program.cs
+++ src/insights/FastEnum.Benchmarks/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿using System;
+using BenchmarkDotNet.Running;
+using FastEnumUtility.Benchmarks.Internals;
+
+
+
+//var config = BenchmarkConfig.ForDefault();
+var config = BenchmarkConfig.ForShortRun();
+var switcher = new BenchmarkSwitcher(Type.EmptyTypes);
+switcher.Run(args, config);

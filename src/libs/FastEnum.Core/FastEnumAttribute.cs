305af16b470b666663c001e22483046d692dc013
Takaaki Suzuki <xin9le@live.jp>
2024-08-20 20:36:47 +0900

    Add marker attributes for source generator

---
+++ src/libs/FastEnum.Core/FastEnumAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FastEnumUtility;
+
+
+
+/// <summary>
+/// A marker attribute utilized to automatically generate high-performance enum utility implementations using Source Generator.
+/// </summary>
+[AttributeUsage(AttributeTargets.Enum)]
+public sealed class FastEnumAttribute : Attribute
+{ }
+
+
+
+/// <summary>
+/// A marker attribute utilized to automatically generate high-performance enum utility implementations using Source Generator.
+/// </summary>
+/// <typeparam name="T"><see cref="Enum"/> type</typeparam>
+[AttributeUsage(AttributeTargets.Class)]
+public sealed class FastEnumAttribute<T> : Attribute
+    where T : struct, Enum
+{ }

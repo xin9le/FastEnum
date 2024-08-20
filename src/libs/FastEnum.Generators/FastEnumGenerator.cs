5a5e4562b791890c0cd556c3f7a7f07d2367ad78
Takaaki Suzuki <xin9le@live.jp>
2024-08-20 20:37:30 +0900

    source code emit sample

--- src/libs/FastEnum.Generators/FastEnumGenerator.cs
+++ src/libs/FastEnum.Generators/FastEnumGenerator.cs
@@ -9,5 +9,34 @@ public sealed class FastEnumGenerator : IIncrementalGenerator
 {
     /// <inheritdoc/>
     public void Initialize(IncrementalGeneratorInitializationContext context)
-    { }
+    {
+        var source = context.SyntaxProvider.ForAttributeWithMetadataName
+        (
+            fullyQualifiedMetadataName: "FastEnumUtility.FastEnumAttribute",
+            predicate: static (node, cancellationToken) => true,
+            transform: static (context, cancellationToken) => context
+        );
+        context.RegisterSourceOutput(source, SourceCodeEmitter.Emit);
+    }
+}
+
+
+
+file static class SourceCodeEmitter
+{
+    public static void Emit(SourceProductionContext context, GeneratorAttributeSyntaxContext source)
+    {
+        const string code = """
+using System;
+
+namespace FastEnumUtility.Generated;
+
+
+
+file static class SampleClass
+{
+}
+""";
+        context.AddSource($"FastEnum.GeneratorSample.g.cs", code);
+    }
 }

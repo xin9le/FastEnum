419b9f56608550a55214ae3cbe58678591d27b66
Takaaki Suzuki <xin9le@live.jp>
2024-08-20 22:16:21 +0900

    add sample code

--- src/insights/FastEnum.Sandbox/Program.cs
+++ src/insights/FastEnum.Sandbox/Program.cs
@@ -1,1 +1,22 @@
-﻿{ }
+﻿using System;
+using FastEnumUtility;
+
+
+var a = FastEnum.TryParse<Fruits>("Apple", out var fruit);
+Console.WriteLine(a);
+Console.WriteLine(fruit);
+
+
+[FastEnum]
+public enum Fruits
+{
+    Apple = 1,
+    Banana,
+}
+
+
+[FastEnum<Fruits>]
+public partial class Foo
+{
+
+}

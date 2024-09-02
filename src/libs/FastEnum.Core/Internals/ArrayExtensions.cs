ce07f8a992d7e5f351bab033e42e34867bd63d49
Takaaki Suzuki <xin9le@live.jp>
2024-09-02 14:41:27 +0900

    No range check indexer

---
+++ src/libs/FastEnum.Core/Internals/ArrayExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace FastEnumUtility.Internals;
+
+
+
+internal static class ArrayExtensions
+{
+    public static ref T At<T>(this T[] array, int index)
+    {
+        // note:
+        //  - `Array[i]` includes a range check, but using `Unsafe` eliminates it.
+        //  - While this increases the risk, it is expected to speed up the process.
+
+        ref var pointer = ref MemoryMarshal.GetArrayDataReference(array);
+        return ref Unsafe.Add(ref pointer, index);
+    }
+}

1164763df3954f3868d93246203fc27affbf11d4
Takaaki Suzuki <xin9le@live.jp>
2024-09-02 14:57:11 +0900

    unsafe indexer for ReadOnlySpan<T>

--- src/libs/FastEnum.Core/Internals/ArrayExtensions.cs
+++ src/libs/FastEnum.Core/Internals/ArrayExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 namespace FastEnumUtility.Internals;
@@ -7,6 +8,17 @@ namespace FastEnumUtility.Internals;
 
 internal static class ArrayExtensions
 {
+    public static ref T At<T>(this ReadOnlySpan<T> span, int index)
+    {
+        // note:
+        //  - `Span[i]` includes a range check, but using `Unsafe` eliminates it.
+        //  - While this increases the risk, it is expected to speed up the process.
+
+        ref var pointer = ref MemoryMarshal.GetReference(span);
+        return ref Unsafe.Add(ref pointer, index);
+    }
+
+
     public static ref T At<T>(this T[] array, int index)
     {
         // note:

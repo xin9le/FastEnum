9e163fd55aa375d42247d11a3655c1f169c604c9
Takaaki Suzuki <xin9le@live.jp>
2024-09-05 10:34:12 +0900

    Add interface for source generator

---
+++ src/libs/FastEnum.Core/IFastEnumBooster.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FastEnumUtility;
+
+
+
+/// <summary>
+/// Provides high performance operation for <see cref="Enum"/> types.
+/// </summary>
+/// <typeparam name="T"><see cref="Enum"/> type</typeparam>
+public interface IFastEnumBooster<T>
+    where T : struct, Enum
+{
+}

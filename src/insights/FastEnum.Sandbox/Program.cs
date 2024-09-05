f649a9594a2e3563c6fb2d594340420227e949a1
Takaaki Suzuki <xin9le@live.jp>
2024-09-05 10:23:16 +0900

    Add sandbox project for source generator

---
+++ src/insights/FastEnum.Sandbox/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using FastEnumUtility;
+using FastEnumUtility.Sandbox;
+
+
+var success = FastEnum.TryParse<Fruits>("Apple", out var fruit);
+Console.WriteLine(success);
+Console.WriteLine(fruit);
+
+
+namespace FastEnumUtility.Sandbox
+{
+    public enum Fruits
+    {
+        Apple = 1,
+        Banana,
+    }
+
+
+    [FastEnum<Fruits>]
+    public partial class FruitsBooster
+    { }
+}
+

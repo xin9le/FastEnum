3a44ad06f22eb0f7a5728bf25bfea3151a541ad7
Takaaki Suzuki <xin9le@live.jp>
2024-09-05 10:23:03 +0900

    Add source generator project

---
+++ src/libs/FastEnum.Generators/FastEnumBoosterGenerator.cs
@@ -0,0 +1,16 @@
+﻿using FastEnumUtility.Generators.Emitters;
+using Microsoft.CodeAnalysis;
+
+namespace FastEnumUtility.Generators;
+
+
+
+[Generator(LanguageNames.CSharp)]
+public sealed class FastEnumBoosterGenerator : IIncrementalGenerator
+{
+    /// <inheritdoc/>
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        ClassDeclarationEmitter.Register(context);
+    }
+}

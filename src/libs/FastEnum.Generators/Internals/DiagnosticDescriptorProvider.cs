3f11a88030ac2cd9b151914a66f1e305a5822b9e
Takaaki Suzuki <xin9le@live.jp>
2024-09-09 17:00:51 +0900

    Add DiagnosticDescriptor definitions

---
+++ src/libs/FastEnum.Generators/Internals/DiagnosticDescriptorProvider.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace FastEnumUtility.Generators.Internals;
+
+
+
+internal static class DiagnosticDescriptorProvider
+{
+    #region Constants
+    const string Category = "FastEnum";
+    #endregion
+
+
+    public static readonly DiagnosticDescriptor MustBePartial
+        = new(
+            id: "FE0001",
+            title: "FastEnum booster type must be partial",
+            messageFormat: "FastEnum booster type '{0}' must be partial",
+            category: Category,
+            defaultSeverity: DiagnosticSeverity.Error,
+            isEnabledByDefault: true
+        );
+}

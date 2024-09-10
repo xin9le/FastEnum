c88b85c09b1065387a4b65edcde9de20ba4ad229
Takaaki Suzuki <xin9le@live.jp>
2024-09-10 16:20:13 +0900

    Add booster test cases (sample)

---
+++ src/insights/FastEnum.UnitTests/Cases/BoosterTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using FastEnumUtility.UnitTests.Models;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FastEnumUtility.UnitTests.Cases;
+
+
+
+#pragma warning disable FE0000  // Experimental
+[TestClass]
+public class BoosterTests
+{
+    [TestMethod]
+    public void GetNames()
+    {
+        //--- defined value
+        {
+            const SByteEnum defined = SByteEnum.MinValue;
+            var expect = Enum.GetName(defined);
+            var actual = FastEnum.GetName<SByteEnum, SByteEnumBooster>(defined);
+            actual.Should().NotBeNull();
+            actual.Should().BeEquivalentTo(expect);
+        }
+        //--- undefined value
+        {
+            const SByteEnum undefined = (SByteEnum)123;
+            var expect = Enum.GetName(undefined);
+            var actual = FastEnum.GetName<SByteEnum, SByteEnumBooster>(undefined);
+            actual.Should().BeNull();
+            actual.Should().BeEquivalentTo(expect);
+        }
+    }
+}
+#pragma warning restore FE0000
---
+++ src/insights/FastEnum.UnitTests/Models/Boosters.cs
@@ -0,0 +1,25 @@
+﻿namespace FastEnumUtility.UnitTests.Models;
+
+
+
+[FastEnum<SByteEnum>]
+public sealed partial class SByteEnumBooster
+{ }
+
+
+
+[FastEnum<ByteEnum>]
+public sealed partial record class ByteEnumBooster
+{ }
+
+
+
+[FastEnum<SameValueEnum>]
+public readonly partial struct SameValueEnumBooster
+{ }
+
+
+
+[FastEnum<EmptyEnum>]
+public readonly partial record struct EmptyEnumBooster
+{ }

d5c550fc7c6cf94ec092b0921cd090c54868f361
Takaaki Suzuki <xin9le@live.jp>
2024-09-10 16:31:18 +0900

    Add empty test case (sample)

---
+++ src/insights/FastEnum.UnitTests/Cases/Generators/EmptyTests.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TBooster = FastEnumUtility.UnitTests.Models.EmptyEnumBooster;
+using TEnum = FastEnumUtility.UnitTests.Models.EmptyEnum;
+
+namespace FastEnumUtility.UnitTests.Cases.Generators;
+
+
+
+[TestClass]
+public class EmptyTests
+{
+    [TestMethod]
+    public void GetName()
+        => FastEnum.GetName<TEnum, TBooster>(default).Should().BeNull();
+}

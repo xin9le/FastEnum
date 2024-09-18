a1d8f64dfcbc1984d5f70cf81f73095c0402f95a
Takaaki Suzuki <xin9le@live.jp>
2024-09-18 12:09:35 +0900

    Add booster types

--- src/insights/FastEnum.UnitTests/Models/Boosters.cs
+++ src/insights/FastEnum.UnitTests/Models/Boosters.cs
@@ -9,7 +9,43 @@ public sealed partial class SByteEnumBooster
 
 
 [FastEnum<ByteEnum>]
-public sealed partial record class ByteEnumBooster
+public sealed partial class ByteEnumBooster
+{ }
+
+
+
+[FastEnum<Int16Enum>]
+public sealed partial class Int16EnumBooster
+{ }
+
+
+
+[FastEnum<UInt16Enum>]
+public sealed partial class UInt16EnumBooster
+{ }
+
+
+
+[FastEnum<Int32Enum>]
+public sealed partial class Int32EnumBooster
+{ }
+
+
+
+[FastEnum<UInt32Enum>]
+public sealed partial class UInt32EnumBooster
+{ }
+
+
+
+[FastEnum<Int64Enum>]
+public sealed partial class Int64EnumBooster
+{ }
+
+
+
+[FastEnum<UInt64Enum>]
+public sealed partial class UInt64EnumBooster
 { }
 
 

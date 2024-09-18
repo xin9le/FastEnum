dee70b4b9270289976975edcf1e2f7cfe1d63b58
Takaaki Suzuki <xin9le@live.jp>
2024-09-19 00:32:04 +0900

    adjust pack.bat

--- nuget/pack.bat
+++ nuget/pack.bat
@@ -1,1 +1,4 @@
+dotnet pack ../src/libs/FastEnum/FastEnum.csproj -c Release -o ./packages
 dotnet pack ../src/libs/FastEnum.Core/FastEnum.Core.csproj -c Release -o ./packages
+dotnet pack ../src/libs/FastEnum.Generators/FastEnum.Generators.csproj -c Release -o ./packages
+pause

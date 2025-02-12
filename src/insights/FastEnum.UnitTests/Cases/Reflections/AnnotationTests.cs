2579f4e7451e59ba17112a81d4220769928543c9
Takaaki Suzuki <xin9le@live.jp>
2025-02-12 22:38:23 +0900

    Move AnnotationTests to Shouldly

--- src/insights/FastEnum.UnitTests/Cases/Reflections/AnnotationTests.cs
+++ src/insights/FastEnum.UnitTests/Cases/Reflections/AnnotationTests.cs
@@ -1,5 +1,5 @@
-﻿using FluentAssertions;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
 using TEnum = FastEnumUtility.UnitTests.Models.AnnotationEnum;
 
 namespace FastEnumUtility.UnitTests.Cases.Reflections;
@@ -11,7 +11,7 @@ public class AnnotationTests
 {
     [TestMethod]
     public void IsFlags()
-        => FastEnum.IsFlags<TEnum>().Should().Be(true);
+        => FastEnum.IsFlags<TEnum>().ShouldBe(true);
 
 
     [TestMethod]
@@ -20,21 +20,21 @@ public class AnnotationTests
         {
             var member = TEnum.Zero.ToMember()!;
             var attr = member.EnumMemberAttribute!;
-            attr.Should().NotBeNull();
-            attr.IsValueSetExplicitly.Should().BeTrue();
-            attr.Value.Should().Be("_zero_");
+            attr.ShouldNotBeNull();
+            attr.IsValueSetExplicitly.ShouldBeTrue();
+            attr.Value.ShouldBe("_zero_");
         }
         {
             var member = TEnum.One.ToMember()!;
             var attr = member.EnumMemberAttribute!;
-            attr.Should().NotBeNull();
-            attr.IsValueSetExplicitly.Should().BeFalse();
-            attr.Value.Should().BeNull();
+            attr.ShouldNotBeNull();
+            attr.IsValueSetExplicitly.ShouldBeFalse();
+            attr.Value.ShouldBeNull();
         }
         {
             var member = TEnum.Two.ToMember()!;
             var attr = member.EnumMemberAttribute!;
-            attr.Should().BeNull();
+            attr.ShouldBeNull();
         }
     }
 
@@ -42,54 +42,42 @@ public class AnnotationTests
     [TestMethod]
     public void GetEnumMemberValue()
     {
-        TEnum.Zero.GetEnumMemberValue(throwIfNotFound: false).Should().Be("_zero_");
-        TEnum.Zero.GetEnumMemberValue(throwIfNotFound: true).Should().Be("_zero_");
+        TEnum.Zero.GetEnumMemberValue(throwIfNotFound: false).ShouldBe("_zero_");
+        TEnum.Zero.GetEnumMemberValue(throwIfNotFound: true).ShouldBe("_zero_");
 
-        TEnum.One.GetEnumMemberValue(throwIfNotFound: false).Should().BeNull();
-        TEnum.One.GetEnumMemberValue(throwIfNotFound: true).Should().BeNull();
+        TEnum.One.GetEnumMemberValue(throwIfNotFound: false).ShouldBeNull();
+        TEnum.One.GetEnumMemberValue(throwIfNotFound: true).ShouldBeNull();
 
-        TEnum.Two.GetEnumMemberValue(throwIfNotFound: false).Should().BeNull();
-        FluentActions
-            .Invoking(static () => TEnum.Two.GetEnumMemberValue(throwIfNotFound: true))
-            .Should()
-            .Throw<NotFoundException>();
+        TEnum.Two.GetEnumMemberValue(throwIfNotFound: false).ShouldBeNull();
+        Should.Throw<NotFoundException>(static () => TEnum.Two.GetEnumMemberValue(throwIfNotFound: true));
 
         const TEnum undefined = (TEnum)123;
-        undefined.GetEnumMemberValue(throwIfNotFound: false).Should().BeNull();
-        FluentActions
-            .Invoking(static () => undefined.GetEnumMemberValue(throwIfNotFound: true))
-            .Should()
-            .Throw<NotFoundException>();
+        undefined.GetEnumMemberValue(throwIfNotFound: false).ShouldBeNull();
+        Should.Throw<NotFoundException>(static () => undefined.GetEnumMemberValue(throwIfNotFound: true));
     }
 
 
     [TestMethod]
     public void GetLabel()
     {
-        TEnum.Zero.GetLabel(0, throwIfNotFound: false).Should().Be("ぜろ");
-        TEnum.Zero.GetLabel(0, throwIfNotFound: true).Should().Be("ぜろ");
-        TEnum.Zero.GetLabel(1, throwIfNotFound: false).Should().Be("零");
-        TEnum.Zero.GetLabel(1, throwIfNotFound: true).Should().Be("零");
+        TEnum.Zero.GetLabel(0, throwIfNotFound: false).ShouldBe("ぜろ");
+        TEnum.Zero.GetLabel(0, throwIfNotFound: true).ShouldBe("ぜろ");
+        TEnum.Zero.GetLabel(1, throwIfNotFound: false).ShouldBe("零");
+        TEnum.Zero.GetLabel(1, throwIfNotFound: true).ShouldBe("零");
 
-        TEnum.One.GetLabel(0, throwIfNotFound: false).Should().Be("いち");
-        TEnum.One.GetLabel(0, throwIfNotFound: true).Should().Be("いち");
-        TEnum.One.GetLabel(1, throwIfNotFound: false).Should().Be("壱");
-        TEnum.One.GetLabel(1, throwIfNotFound: true).Should().Be("壱");
+        TEnum.One.GetLabel(0, throwIfNotFound: false).ShouldBe("いち");
+        TEnum.One.GetLabel(0, throwIfNotFound: true).ShouldBe("いち");
+        TEnum.One.GetLabel(1, throwIfNotFound: false).ShouldBe("壱");
+        TEnum.One.GetLabel(1, throwIfNotFound: true).ShouldBe("壱");
 
-        TEnum.Two.GetLabel(0, throwIfNotFound: false).Should().BeNull();
-        FluentActions
-            .Invoking(static () => TEnum.Two.GetLabel(0, throwIfNotFound: true))
-            .Should()
-            .Throw<NotFoundException>();
+        TEnum.Two.GetLabel(0, throwIfNotFound: false).ShouldBeNull();
+        Should.Throw<NotFoundException>(static () => TEnum.Two.GetLabel(0, throwIfNotFound: true));
 
-        TEnum.Four.GetLabel(2, throwIfNotFound: false).Should().BeNull();
-        TEnum.Four.GetLabel(2, throwIfNotFound: true).Should().BeNull();
+        TEnum.Four.GetLabel(2, throwIfNotFound: false).ShouldBeNull();
+        TEnum.Four.GetLabel(2, throwIfNotFound: true).ShouldBeNull();
 
         const TEnum undefined = (TEnum)123;
-        undefined.GetLabel(0, throwIfNotFound: false).Should().BeNull();
-        FluentActions
-            .Invoking(static () => undefined.GetLabel(0, throwIfNotFound: true))
-            .Should()
-            .Throw<NotFoundException>();
+        undefined.GetLabel(0, throwIfNotFound: false).ShouldBeNull();
+        Should.Throw<NotFoundException>(static () => undefined.GetLabel(0, throwIfNotFound: true));
     }
 }
